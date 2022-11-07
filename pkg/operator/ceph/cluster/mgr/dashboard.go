654b831e8198b38c1b938b24c2428f8643b6da41
Alexander Trost <galexrt@googlemail.com>
2022-11-07 13:05:49 +0100

    mgr: initial sso structures
    Signed-off-by: Alexander Trost <galexrt@googlemail.com>

--- pkg/operator/ceph/cluster/mgr/dashboard.go
+++ pkg/operator/ceph/cluster/mgr/dashboard.go
@@ -102,6 +102,8 @@ func (c *Cluster) configureDashboardModules() error {
 			hasChanged = true
 		}
 	}
+	c.setupSSO()
+
 	if hasChanged {
 		logger.Info("dashboard config has changed. restarting the dashboard module")
 		return c.restartDashboard()
@@ -314,4 +316,38 @@ func (c *Cluster) restartDashboard() error {
 		return errors.Wrapf(err, "failed to enable mgr module %q.", dashboardModuleName)
 	}
 	return nil
+}
+
+func (c *Cluster) setupSSO() (bool, error) {
+	if !c.spec.Dashboard.SSO.Enabled {
+		// TODO make sure SSO is disabled
+		return false, nil
+	}
+	// TODO check if we need to "re-setup" SSO
+
+	// create a self-signed cert for the https connections
+	args := []string{"dashboard", "sso", "setup"}
+	// TODO build sso setup args command
+
+	// retry a few times in the case that the mgr module is not ready to accept commands
+	for i := 0; i < 5; i++ {
+		_, err := client.NewCephCommand(c.context, c.clusterInfo, args).RunWithTimeout(exec.CephCommandsTimeout)
+		if err == context.DeadlineExceeded {
+			logger.Warning("sso setup timed out. trying again")
+			continue
+		}
+		if err != nil {
+			exitCode, parsed := c.exitCode(err)
+			if parsed {
+				if exitCode == invalidArgErrorCode {
+					logger.Info("dashboard module is not ready yet. trying again")
+					time.Sleep(dashboardInitWaitTime)
+					continue
+				}
+			}
+			return false, errors.Wrap(err, "failed to setup sso on mgr dashboard")
+		}
+		break
+	}
+	return false, nil
 }
