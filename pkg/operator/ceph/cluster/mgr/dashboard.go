e3125244134cf6aa48f97f127d8b4f39a06ed520
gauravsitlani <gauravsitlani@riseup.net>
2023-02-14 21:24:11 +0530

    refactoring the sso code
    updated some changes and cleaned up the commented code
    
    Signed-off-by: gauravsitlani <gauravsitlani@riseup.net>

--- pkg/operator/ceph/cluster/mgr/dashboard.go
+++ pkg/operator/ceph/cluster/mgr/dashboard.go
@@ -340,24 +340,12 @@ func (c *Cluster) setupSSO() (bool, error) {
 		}
 		return false, nil
 	}
-	// TODO create and build sso setup args command
+	// create and build sso setup args command
 	dashboardUrl := c.spec.Dashboard.SSO.BaseURL
 	idpMetadataUrl := c.spec.Dashboard.SSO.IDPMetadataUrl
 	args := []string{"dashboard", "sso", "setup", "saml2", dashboardUrl, idpMetadataUrl}
 	idpUsernameAttribute := c.spec.Dashboard.SSO.IDPAttributes.Username
 	idpEntityId := c.spec.Dashboard.SSO.EntityID
-	// spCertificateKey := c.spec.Dashboard.SSO.SPCert.Key
-	// spCertificateSecretname := c.spec.Dashboard.SSO.SPCert.SecretName
-	// spPrivateKeyname := c.spec.Dashboard.SSO.SPPrivateKey.Key
-	// spPrivateKeysecret := c.spec.Dashboard.SSO.SPPrivateKey.SecretName
-
-	// users_len := len(c.spec.Dashboard.SSO.Users)
-	// Objective: To Get the list of Users and their respective roles from the struct
-	// And create them with their roles
-	// for i := 0; i < users_len; i++ {
-	// 	userlist := c.spec.Dashboard.SSO.Users
-
-	// }
 
 	if idpUsernameAttribute != "" || idpEntityId != "" {
 		args = []string{"dashboard", "sso", "setup", dashboardUrl, idpMetadataUrl, idpUsernameAttribute, idpEntityId}
