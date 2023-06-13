e248484b4c37afc9791e7e01e89ff701330f7f2b
Alexander Trost <galexrt@googlemail.com>
2023-06-13 14:22:29 +0200

    ci: tweak rook rebase script
    Signed-off-by: Alexander Trost <galexrt@googlemail.com>

--- ee/scripts/merge_rook_master.sh
+++ ee/scripts/merge_rook_master.sh
@@ -1,26 +1,57 @@
 #!/bin/bash
 
-set -x
-
-ROOK_BRANCH="master"
+set +x
+set -e
 
 if [[ -z "$GITHUB_TOKEN" ]]; then
-	echo "Set the GITHUB_TOKEN env variable."
-	exit 1
+    echo "Set the GITHUB_TOKEN env variable."
+    exit 1
 fi
 
-git remote add rook_fork https://github.com/rook/rook.git
-git fetch rook_fork $ROOK_BRANCH
+set -x
 
-set -o xtrace
+export REMOTE_NAME="${REMOTE_NAME:-rook_fork}"
+export SOURCE_BRANCH="${SOURCE_BRANCH:-master}"
+export TARGET_BRANCH="${TARGET_BRANCH:-merge-rook-master}"
 
-# do the merge
-git checkout -b merge-rook-master
+if git remote | grep --quiet "$REMOTE_NAME"; then
+    git remote set-url "$REMOTE_NAME" https://github.com/rook/rook.git
+else
+    git remote add "$REMOTE_NAME" https://github.com/rook/rook.git
+fi
 
-git merge rook_fork/$ROOK_BRANCH
-#specific files that will be completely ours
-git checkout --ours rook_fork/master -- README.md
-(git -c core.editor=/bin/true merge --continue) || (echo "Merge failed, try merging manual" && exit 1)
+# Fetch latest rook branch and switch to our rebase branch
+# (if it doesn't exist locally yet, it will be reset)
+git fetch "$REMOTE_NAME" "$SOURCE_BRANCH"
+git checkout -B "$TARGET_BRANCH"
 
-# push back
-git push --force-with-lease origin merge-rook-master
+# Run the merge (ignore any failures)
+git merge "$REMOTE_NAME/$SOURCE_BRANCH" || true
+
+# Specific files/dirs that will be completely ours
+declare -a OUR_FILES=(
+    README.md
+    Documentation/
+)
+
+for OUR_FILE in "${OUR_FILES[@]}"; do
+    git checkout \
+        --ours \
+        "$REMOTE_NAME/master" -- "$OUR_FILE"
+done
+
+# Make sure to re-generate CRDs, Helm Chart Docs, etc., to be up-to-date with custom changes
+make crds helm-docs validate-and-gen-docs-crds
+# Stage the re-generated files
+git add \
+    deploy/charts/ \
+    deploy/olm/ \
+    Documentation/
+
+git \
+    -c core.editor=/bin/true \
+    merge --continue || \
+    { echo "Merge failed, try merging manual"; exit 1; }
+
+# Push back
+git push --force-with-lease origin "$TARGET_BRANCH"
